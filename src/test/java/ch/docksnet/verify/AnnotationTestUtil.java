862422395c14f4efc4e2df252c0e46ada30cab0e
Stefan Zeller <stefan.zeller@docksnet.ch>
2014-09-17 14:18:47 +0200

    provide support for @Size

--- src/test/java/ch/docksnet/verify/AnnotationTestUtil.java
+++ src/test/java/ch/docksnet/verify/AnnotationTestUtil.java
@@ -3,6 +3,7 @@ package ch.docksnet.verify;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.lang.annotation.Annotation;
 
 /**
@@ -25,6 +26,14 @@ public class AnnotationTestUtil {
         return TestInterface.class.getDeclaredMethod("withMin10", Long.class).getParameterAnnotations()[0][0];
     }
 
+    public static Annotation getAnnotationSizeMin1() throws NoSuchMethodException {
+        return TestInterface.class.getDeclaredMethod("withSizeMin1", Long.class).getParameterAnnotations()[0][0];
+    }
+
+    public static Annotation getAnnotationSizeMax3() throws NoSuchMethodException {
+        return TestInterface.class.getDeclaredMethod("withSizeMax3", Long.class).getParameterAnnotations()[0][0];
+    }
+
     @SuppressWarnings("UnusedDeclaration")
     public interface TestInterface {
         void withNotNull(@NotNull String arg1);
@@ -34,5 +43,9 @@ public class AnnotationTestUtil {
         void withMaxMinus10(@Max(value = -10) Long l);
 
         void withMin10(@Min(value = 10) Long l);
+
+        void withSizeMin1(@Size(min = 1) Long l);
+
+        void withSizeMax3(@Size(max = 3) Long l);
     }
 }
